991fff9719d6e3ebd51401fe23ea4119468978a7
10252268196 <dd2321-*->
2023-05-25 14:58:22 +0300

    logging and exception

--- setup.py
+++ setup.py
@@ -1,25 +1,24 @@
 #building our application as a package
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
+HYPEN_E_DOT = '-e .'
 
-HYPEN_E_DOT='-e .'
 
-def get_requirements(file:str)->List[str]:
+def get_requirements(file_path: str) -> List[str]:
     '''
     this function will return the list of requirements
-    :param file:
-    :return:
     '''
-
-    requirements=[]
-    with open('requirements.txt') as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
-            requirements.remove((HYPEN_E_DOT))
+            requirements.remove(HYPEN_E_DOT)
+
     return requirements
+
 
 setup(
     name='mlproject',
@@ -27,5 +26,6 @@ setup(
     author='İsmail Akgül',
     author_email='ismailakgul.ia@gmail.com',
     packages=find_packages(),
-    install_requires= get_requirements('requirements.txt')
+    install_requires=get_requirements('requirements.txt')
+
 )
